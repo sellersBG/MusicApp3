68473e1646107f6ebccb623e73834cb6b821346f
sellersBG <tsellers@barnegatschools.com>
2019-04-02 13:13:03 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/tsellers/musicapp3/Clarinet.java
@@ -0,0 +1,12 @@
+package com.example.tsellers.musicapp3;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Clarinet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clarinet);
+    }
+}
---
+++ app/src/main/java/com/example/tsellers/musicapp3/FrenchHorn.java
@@ -0,0 +1,13 @@
+package com.example.tsellers.musicapp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrenchHorn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frenchhorn);
+    }
+}
